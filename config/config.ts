954993422fd074bb00843e954607a9b96c61b951
yogirpatel <yogirpatel@gmail.com>
2018-03-13 22:56:08 +0530

    Update config.ts

--- config/config.ts
+++ config/config.ts
@@ -1,9 +1,11 @@
 import {browser, Config} from 'protractor';
+import * as fs from 'fs-extra';
 
 export let config: Config = {
-    baseUrl: process.env['PTOR_BASE_URL'] || 'https://beta.formed.org',
+    baseUrl: process.env['PTOR_BASE_URL'] || 'https://market.beta.formed.org',
     getPageTimeout: 60000,
     allScriptsTimeout: 500000,
+    rootElement: 'body',
   // seleniumAddress: 'http://localhost:4444/wd/hub',
 
     SELENIUM_PROMISE_MANAGER: false,
@@ -17,13 +19,13 @@ export let config: Config = {
       //  'browserName': 'firefox',
         //'firefoxOptions': {
         // 'args': ['--headless']
-     //   },
+        //},
       // 'moz:firefoxOptions': {
         //    'binary': '/var/lib/jenkins/workspace/Regression_tests/node_modules/webdriver-manager/selenium/geckodriver-v0.19.1/'
         // }},
-   // {
-     //   'browserName': 'chrome'
-     // }],
+    //{
+      //  'browserName': 'chrome'
+      //}],
      // {'browserName': 'internet explorer',
      //   'platform': 'ANY',
      //   'version': '11'
@@ -41,7 +43,20 @@ export let config: Config = {
     ],
 
     onPrepare: () => {
+        browser.executeScript('return !!window.angular');
         browser.ignoreSynchronization = true;
+        browser.ignoreUncaughtExceptions =  true;
+        browser.waitForAngularEnabled(false);
+        browser.sleep(500);
+        let oldreport = process.cwd() +"/.tmp/report/index.html";
+        var now = new Date();
+        
+        var logfile_name = "index-"+ now.getFullYear() + "-"+ now.getMonth() + "-" + now.getDay()+"-"+now.getHours()+"-"+now.getMinutes()+"-"+now.getSeconds() +'.html';
+        let __dirname = process.cwd();
+        if(fs.existsSync(oldreport)) {
+            fs.copySync(oldreport,__dirname+"/archive-Reports/"+logfile_name);
+            fs.unlinkSync(oldreport);
+        }
         browser.manage().window().setSize(1600, 1000);
     },
     cucumberOpts: {
@@ -61,7 +76,3 @@ export let config: Config = {
         }
     }]
 };
-
-//function newFunction(): string {
-  //  return 'chrome';
-//}
